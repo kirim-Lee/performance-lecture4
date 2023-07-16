c0c514a1bb8c98feaacd28d67740171da51815ed
이기림(Kirim Lee)/Biz플랫폼개발팀/SKP <kirim@sk.com>
2023-07-16 13:40:53 +0900

    after 최적화

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
-import { showModal } from '../redux/imageModal';
+import { setBgColor, showModal } from '../redux/imageModal';
+import LazyLoad from 'react-lazyload';
+import { getAverageColorOfImage } from '../utils/getAverageColorOfImage';
 
 function PhotoItem({ photo: { urls, alt } }) {
   const dispatch = useDispatch();
 
-  const openModal = () => {
+  const openModal = e => {
     dispatch(showModal({ src: urls.full, alt }));
+
+    const averageColor = getAverageColorOfImage(e.target);
+    dispatch(setBgColor(averageColor));
   };
 
   return (
     <ImageWrap>
-      <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      <LazyLoad offset={1000}>
+        <Image
+          src={urls.small + '&t=' + new Date().getTime()}
+          crossOrigin="anonymous"
+          alt={alt}
+          onClick={openModal}
+        />
+      </LazyLoad>
     </ImageWrap>
   );
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  position: relative;
+  width: 100%;
+  padding-top: 56.25%;
+`;
 
 const Image = styled.img`
   cursor: pointer;
+  position: absolute;
   width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
 `;
 
 export default PhotoItem;
